463defb583256388df3a13d6c9324b402d703254
Karissa Bui <karissab002@gmail.com>
2018-07-30 16:18:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,3 +9,19 @@
 # end 
 
 # example of methods
+
+class Dog 
+  def name = (dog_name)
+    @this_dogs_name = 
+dogs_name 
+  end 
+  
+  def name 
+    @this_dogs_name
+  end 
+end
+
+lassie = Dog.new 
+lassie.name = "Lassie" 
+
+puts lassie.name
